a79a6574abf5e10afbae196e5b2a94802dd0ca27
Linh Truong Cong Hong <67230802+linhtch90@users.noreply.github.com>
2023-02-05 12:08:59 +0700

    Init backend

---
+++ src/main/java/com/aptech/sem4eprojectbe/Sem4EprojectBeApplication.java
@@ -0,0 +1,13 @@
+package com.aptech.sem4eprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4EprojectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem4EprojectBeApplication.class, args);
+	}
+
+}
