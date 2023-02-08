cd491988da6b2a91f7c962c5116de617400f8a21
Linh Truong Cong Hong <67230802+linhtch90@users.noreply.github.com>
2023-02-08 20:03:24 +0700

    Create basic CRUD faq

---
+++ src/main/java/com/aptech/sem4eprojectbe/common/model/IdModel.java
@@ -0,0 +1,12 @@
+package com.aptech.sem4eprojectbe.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IdModel {
+    private String id;
+}
---
+++ src/main/java/com/aptech/sem4eprojectbe/service/FaqService.java
@@ -0,0 +1,50 @@
+package com.aptech.sem4eprojectbe.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aptech.sem4eprojectbe.common.model.IdModel;
+import com.aptech.sem4eprojectbe.common.model.ResponseModel;
+import com.aptech.sem4eprojectbe.entity.FaqEntity;
+import com.aptech.sem4eprojectbe.repository.FaqRepository;
+
+@Service
+public class FaqService {
+    @Autowired
+    private FaqRepository faqRepository;
+
+    public FaqEntity insertFaq(FaqEntity faq) {
+        return faqRepository.save(faq);
+    }
+
+    public List<FaqEntity> getAllFaqs() {
+        return faqRepository.findAll();
+    }
+
+    public Optional<FaqEntity> getFaqById(IdModel idModel) {
+        return faqRepository.findById(idModel.getId());
+    }
+
+    public FaqEntity updateFaq(FaqEntity faq) {
+        return faqRepository.findById(faq.getId())
+                .map(faqItem -> {
+                    faqItem.setQuestion(faq.getQuestion());
+                    faqItem.setAnswer(faq.getAnswer());
+                    return faqRepository.save(faqItem);
+                })
+                .orElseGet(() -> {
+                    FaqEntity newFaq = new FaqEntity();
+                    newFaq.setQuestion(faq.getQuestion());
+                    newFaq.setAnswer(faq.getAnswer());
+                    newFaq.setDeleted(false);
+                    return faqRepository.save(newFaq);
+                });
+    }
+
+    public void removeFaqById(IdModel idModel) {
+        faqRepository.deleteById(idModel.getId());
+    }
+}
